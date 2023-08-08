4b9968a5555ae62fec5e4a9dfe6f184b6d12bccc
amrylnikov <amrylnikov@gmail.com>
2023-08-08 10:25:03 +0300

    added login, logout, registration

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from task_manager.users import views
 
 urlpatterns = [
-    path('', views.index),
-    path('login_user', views.login, name='login'),
+    path('', views.show_users, name='users'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
+    path('create', views.register_user, name='registration'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,11 +1,52 @@
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+
+from task_manager.users.forms import RegisterUserForm
 
 
-def index(request):
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('Вы залогинены'))
+            return redirect('index')
+        else:
+            messages.error(request, ('''Пожалуйста, введите правильные имя
+                                       пользователя и пароль. Оба поля могут
+                                       быть чувствительны к регистру.'''))
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('Вы разлогинены'))
+    return redirect('index')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ("Registration Successful!"))
+            return redirect('login')
+        messages.error(request, (form.errors))
+        return render(request, 'registration.html', {
+            'form': form,
+        })
+    else:
+        form = RegisterUserForm()
+        return render(request, 'registration.html', {
+            'form': form,
+        })
+
+
+def show_users(request):
     return render(request, 'users.html')
-
-
-def login(request):
-    return render(request, 'login.html')
