e4e22b90b90fba77f4cee57a4e920f85cfaee11c
amrylnikov <amrylnikov@gmail.com>
2023-08-25 10:26:39 +0300

    removed re_path

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 
 from task_manager.users import views
-from django.urls import re_path
 
 urlpatterns = [
     path('', views.UsersShow.as_view(), name='users'),
     path('create/', views.UserRegister.as_view(), name='registration'),
     # path('<int:pk>/edit/', views.UserEdit.as_view(), name='edit'),
-    re_path(r'^(?P<pk>\d+)/edit/$', views.UserEdit.as_view(), name='edit'),
+    path(r'^(?P<pk>\d+)/edit/$', views.UserEdit.as_view(), name='edit'),
     path('<int:pk>/delete/', views.UserDelete.as_view(), name='delete'),
 ]
