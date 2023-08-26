c1f67cb3753c1827eec8eb291ef7fd237ac06d60
amrylnikov <amrylnikov@gmail.com>
2023-08-26 16:29:28 +0300

    updated filter

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,10 +1,17 @@
 import django_filters
+from django.contrib.auth.models import User
+
+from task_manager.statuses.models import Status
 from task_manager.tasks.models import Task
+from task_manager.labels.models import Label
 
 
 class TaskFilter(django_filters.FilterSet):
-    username = django_filters.CharFilter(method='my_custom_filter')
-    author = django_filters.BooleanFilter
+    # username = django_filters.CharFilter(method='my_custom_filter')
+    # author = django_filters.BooleanFilter
+    status = django_filters.ModelChoiceFilter(queryset=Status.objects.all(), label='Статус')
+    executor = django_filters.ModelChoiceFilter(queryset=User.objects.all(), label='Исполнитель')
+    labels = django_filters.ModelMultipleChoiceFilter(queryset=Label.objects.all(), label='Метки')
 
     class Meta:
         model = Task
