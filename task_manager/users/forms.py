bdd7256adee479f4b76f513b8b25cb1c2177aef3
amrylnikov <amrylnikov@gmail.com>
2023-08-26 13:06:44 +0300

    changed registration html

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -7,9 +7,22 @@ from django import forms
 class RegisterUserForm(UserCreationForm):
     first_name = forms.CharField(max_length=50, label='Имя')
     last_name = forms.CharField(max_length=50, label='Фамилия')
-    username = forms.CharField(max_length=50, label='Имя пользователя')
-    password1 = forms.CharField(max_length=50, label='Пароль')
-    password2 = forms.CharField(max_length=50, label='Подтверждение пароля')
+    username = forms.CharField(
+        max_length=50,
+        label='Имя пользователя',
+        help_text='''Обязательное поле. Не более 150 символов. Только буквы,
+        цифры и символы @/./+/-/_.'''
+        )
+    password1 = forms.CharField(
+        max_length=50,
+        label='Пароль',
+        help_text='Ваш пароль должен содержать как минимум 3 символа.'
+        )
+    password2 = forms.CharField(
+        max_length=50,
+        label='Подтверждение пароля',
+        help_text='Для подтверждения введите, пожалуйста, пароль ещё раз.'
+        )
     date_joined = DateTimeField(auto_now_add=True)
 
     class Meta:
