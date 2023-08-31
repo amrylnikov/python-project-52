abf15d120e71ef8d59f6a47db5b9f5f9e3d9057e
amrylnikov <amrylnikov@gmail.com>
2023-08-31 15:34:51 +0300

    removed IndexView and replaced with TemplateView

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 from task_manager import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    # path('', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name="index.html"), name='index'),
     path('login/', views.UserLogin.as_view(), name='login'),
     path('logout/', views.UserLogout.as_view(), name='logout'),
     path('users/', include('task_manager.users.urls')),
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,17 +1,9 @@
-from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
 from django.utils.translation import gettext as _
-from django.views import View
 
 from task_manager.mixins import SpecifiedLoginRequiredMixin
-
-
-class IndexView(View):
-
-    def get(self, request, *args, **kwargs):
-        return render(request, 'index.html')
 
 
 class UserLogin(LoginView):
