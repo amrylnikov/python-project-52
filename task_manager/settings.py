a2bf00aa3bff6ced681603c73144b26cb94116c2
amrylnikov <amrylnikov@gmail.com>
2023-09-01 10:57:52 +0300

    changed allowed hosts again

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG')
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-ALLOWED_HOSTS = '*'
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
